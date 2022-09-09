4740d0095e7455a22bbb0d85a2bf4c4ba11dfa44
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-09 11:25:22 +0530

    update and fix bug

--- src/string_view.c
+++ src/string_view.c
@@ -5,327 +5,350 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 StringView svFromParts(const char* data, size_t count)
 {
-    StringView sv;
-    sv.m_data = data;
-    sv.m_count = count;
-    return sv;
+	StringView sv;
+	sv.m_data  = data;
+	sv.m_count = count;
+	return sv;
 }
 
 StringView svFromCStr(const char* cstr)
 {
-    // returns a view of the string, or an empty view if the string is NULL
-    return svFromParts(cstr, cstr ? strlen(cstr) : 0);
+	// returns a view of the string, or an empty view if the string is NULL
+	return svFromParts(cstr, cstr ? strlen(cstr) : 0);
 }
 
 StringView svTrim(StringView sv)
 {
-    // returns a view of the string, or an empty view if the string is NULL
-    if (sv.m_data == NULL)
-        return sv;
+	// returns a view of the string, or an empty view if the string is NULL
+	if(sv.m_data == NULL) return sv;
 
-    // trim leading whitespace
-    while (sv.m_count > 0 && sv.m_data[0] == ' ')
-    {
-        sv.m_data++;
-        sv.m_count--;
-    }
+	// trim leading whitespace
+	while(sv.m_count > 0 && sv.m_data[0] == ' ')
+	{
+		sv.m_data++;
+		sv.m_count--;
+	}
 
-    // trim trailing whitespace
-    while (sv.m_count > 0 && sv.m_data[sv.m_count - 1] == ' ')
-        sv.m_count--;
+	// trim trailing whitespace
+	while(sv.m_count > 0 && sv.m_data[sv.m_count - 1] == ' ') sv.m_count--;
 
-    return sv;
+	return sv;
 }
 
 /*
-    *   @brief: Another Possible Implementation of svTrim
+	*   @brief: Another Possible Implementation of svTrim
 
-    StringView svTrim(StringView sv)
-    {
-        size_t i = 0;
-        while(i < sv.m_count && sv.m_data[i] == ' ')
-            i++;
-    return svFromParts(sv.m_data + i, sv.m_count - i);
-    }
+	StringView svTrim(StringView sv)
+	{
+		size_t i = 0;
+		while(i < sv.m_count && sv.m_data[i] == ' ')
+			i++;
+	return svFromParts(sv.m_data + i, sv.m_count - i);
+	}
 
-    * @brief: This implementation is less efficient and creates a copy of sv , but it is more readable.
+	* @brief: This implementation is less efficient and creates a copy of sv , but it is more
+   readable.
 */
 
 StringView svTrimLeft(StringView sv)
 {
-    // returns a view of the string, or an empty view if the string is NULL
-    if (sv.m_data == NULL)
-        return sv;
+	// returns a view of the string, or an empty view if the string is NULL
+	if(sv.m_data == NULL) return sv;
 
-    // trim leading whitespace
-    while (sv.m_count > 0 && sv.m_data[0] == ' ')
-    {
-        sv.m_data++;
-        sv.m_count--;
-    }
+	// trim leading whitespace
+	while(sv.m_count > 0 && sv.m_data[0] == ' ')
+	{
+		sv.m_data++;
+		sv.m_count--;
+	}
 
-    return sv;
+	return sv;
 }
 
 /*
-    *   @brief: This is another possible implementation for svTrimLeft,
-            but is less efficient and creates a copy of sv ,
-            but it is more readable.
+	*   @brief: This is another possible implementation for svTrimLeft,
+			but is less efficient and creates a copy of sv ,
+			but it is more readable.
 
-    StringView svTrimLeft(StringView sv)
-    {
-        size_t i = 0;
-        while(i < sv.m_count && sv.m_data[i] == ' ')
-            i++;
+	StringView svTrimLeft(StringView sv)
+	{
+		size_t i = 0;
+		while(i < sv.m_count && sv.m_data[i] == ' ')
+			i++;
 
-        return svFromParts(sv.m_data + i, sv.m_count - i);
-    }
+		return svFromParts(sv.m_data + i, sv.m_count - i);
+	}
 
 */
 
 StringView svTrimRight(StringView sv)
 {
-    // returns a view of the string, or an empty view if the string is NULL
-    if (sv.m_data == NULL)
-        return sv;
+	// returns a view of the string, or an empty view if the string is NULL
+	if(sv.m_data == NULL) return sv;
 
-    // trim trailing whitespace
-    while (sv.m_count > 0 && sv.m_data[sv.m_count - 1] == ' ')
-        sv.m_count--;
+	// trim trailing whitespace
+	while(sv.m_count > 0 && sv.m_data[sv.m_count - 1] == ' ') sv.m_count--;
 
-    return sv;
+	return sv;
 }
 
 /*
-    *   @brief: This is another possible implementation for svTrimRight,
-            but is less efficient and creates a copy of sv ,
-            but it is more readable.
+	*   @brief: This is another possible implementation for svTrimRight,
+			but is less efficient and creates a copy of sv ,
+			but it is more readable.
 
-    StringView svTrimRight(StringView sv)
-    {
-        size_t i = 0;
-        while(i < sv.m_count && sv.m_data[i] == ' ')
-            i++;
+	StringView svTrimRight(StringView sv)
+	{
+		size_t i = 0;
+		while(i < sv.m_count && sv.m_data[i] == ' ')
+			i++;
 
-        return svFromParts(sv.m_data, sv.m_count - i);
-    }
+		return svFromParts(sv.m_data, sv.m_count - i);
+	}
 */
 
 StringView svTakeLeftWhile(StringView sv, bool (*predicate)(char x))
 {
-    // returns a view of the string, or an empty view if the string is NULL
-    if (sv.m_data == NULL)
-        return sv;
-
-    // trim leading whitespace
-    while (sv.m_count > 0 && predicate(sv.m_data[0]))
-    {
-        sv.m_data++;
-        sv.m_count--;
-    }
-
-    return sv;
+	size_t i = 0;
+	while(i < sv.m_count && predicate(sv.m_data[i]))
+	{
+		i += 1;
+	}
+	return svFromParts(sv.m_data, i);
 }
 
 /*
-    *   @brief: This is another possible implementation for svTakeLeftWhile,
-            but is less efficient and creates a copy of sv,
-            but it is more readable.
+	*   @brief: This is another possible implementation for svTakeLeftWhile,
+			but is less efficient and creates a copy of sv,
+			but it is more readable.
 
-    StringView svTakeLeftWhile(StringView sv, bool (*predicate)(char x))
-    {
-        size_t i = 0;
-        while(i < sv.m_count && predicate(sv.m_data[i]))
-            i++;
+	StringView svTakeLeftWhile(StringView sv, bool (*predicate)(char x))
+	{
+		size_t i = 0;
+		while(i < sv.m_count && predicate(sv.m_data[i]))
+			i++;
 
-        return svFromParts(sv.m_data, i);
-    }
+		return svFromParts(sv.m_data, i);
+	}
 */
 
 bool try_svChopByDelim(StringView* sv, char delim, StringView* chunk)
 {
-    size_t i = 0;
-    while (i < sv->m_count && sv->m_data[i] != delim)
-        i += 1;
+	size_t i = 0;
+	while(i < sv->m_count && sv->m_data[i] != delim) i += 1;
 
-    StringView result = svFromParts(sv->m_data, i);
+	StringView result = svFromParts(sv->m_data, i);
 
-    if (i < sv->m_count)
-    {
-        sv->m_count -= i + 1;
-        sv->m_data  += i + 1;
-        if (chunk)
-            *chunk = result;
+	if(i < sv->m_count)
+	{
+		sv->m_count -= i + 1;
+		sv->m_data += i + 1;
+		if(chunk) *chunk = result;
 
-        return true;
-    }
+		return true;
+	}
 
-    return false;
+	return false;
 }
-
 
 StringView svChopByDelim(StringView* sv, char delim)
 {
-    size_t i = 0;
-    while (i < sv->m_count && sv->m_data[i] != delim) {
-        i += 1;
-    }
+	size_t i = 0;
+	while(i < sv->m_count && sv->m_data[i] != delim)
+	{
+		i += 1;
+	}
 
-    StringView result = svFromParts(sv->m_data, i);
+	StringView result = svFromParts(sv->m_data, i);
 
-    if (i < sv->m_count) {
-        sv->m_count -= i + 1;
-        sv->m_data  += i + 1;
-    }
-    else
-    {
-        sv->m_count -= i;
-        sv->m_data  += i;
-    }
+	if(i < sv->m_count)
+	{
+		sv->m_count -= i + 1;
+		sv->m_data += i + 1;
+	} else {
+		sv->m_count -= i;
+		sv->m_data += i;
+	}
 
-    return result;
+	return result;
+}
+
+StringView svChopBySV(StringView* sv, StringView thicc_delim)
+{
+	StringView window = svFromParts(sv->m_data, thicc_delim.m_count);
+	size_t i		  = 0;
+	while(i + thicc_delim.m_count < sv->m_count && !(svIsEqual(window, thicc_delim)))
+	{
+		i++;
+		window.m_data++;
+	}
+
+	StringView result = svFromParts(sv->m_data, i);
+
+	if(i + thicc_delim.m_count == sv->m_count)
+	{
+		// include last <thicc_delim.m_count> characters if they aren't
+		//  equal to thicc_delim
+		result.m_count += thicc_delim.m_count;
+	}
+
+	// Chop!
+	sv->m_data += i + thicc_delim.m_count;
+	sv->m_count -= i + thicc_delim.m_count;
+
+	return result;
 }
 
 /*
-    *   @brief: the following implementations is never tested,
-        but it might be more efficient over readability.
+	*   @brief: the following implementations is never tested,
+		but it might be more efficient over readability.
 
-    Todo: test the following implementation.
-    StringView svChopByDelim_2(StringView* sv, char delim)
-    {
-    StringView chunk;
-    if (try_svChopByDelim(&sv, delim, &chunk))
-        return chunk;
-    else
-        return *sv;
-    }
+	Todo: test the following implementation.
+	StringView svChopByDelim_2(StringView* sv, char delim)
+	{
+	StringView chunk;
+	if (try_svChopByDelim(&sv, delim, &chunk))
+		return chunk;
+	else
+		return *sv;
+	}
 */
 
 StringView svChopLeft(StringView* sv, size_t count)
 {
-    if(sv->m_count < count)
-        count = sv->m_count;
+	if(sv->m_count < count) count = sv->m_count;
 
-    StringView result = svFromParts(sv->m_data, count);
+	StringView result = svFromParts(sv->m_data, count);
 
-    sv->m_data += count;
-    sv->m_count -= count;
+	sv->m_data += count;
+	sv->m_count -= count;
 
-    return result;
+	return result;
 }
 
 StringView svChopRight(StringView* sv, size_t count)
 {
-    if(sv->m_count < count)
-        count = sv->m_count;
+	if(sv->m_count < count) count = sv->m_count;
 
-    StringView result = svFromParts(sv->m_data + sv->m_count - count, count);
+	StringView result = svFromParts(sv->m_data + sv->m_count - count, count);
 
-    sv->m_count -= count;
+	sv->m_count -= count;
 
-    return result;
+	return result;
 }
 
 StringView svChopLeftWhile(StringView* sv, bool (*predicate)(char x))
 {
-    size_t i = 0;
-    while (i < sv->m_count && predicate(sv->m_data[i]))
-        i += 1;
+	size_t i = 0;
+	while(i < sv->m_count && predicate(sv->m_data[i])) i += 1;
 
-    return svChopLeft(sv, i);
+	return svChopLeft(sv, i);
 }
 
 bool svIndexOf(StringView sv, char c, size_t* index)
 {
-    size_t i = 0;
-    while (i < sv.m_count && sv.m_data[i] != c)
-        i += 1;
+	size_t i = 0;
+	while(i < sv.m_count && sv.m_data[i] != c) i += 1;
 
-    if (i < sv.m_count)
-    {
-        if (index)
-            *index = i;
-        return true;
-    }
+	if(i < sv.m_count)
+	{
+		if(index) *index = i;
+		return true;
+	}
 
-    return  false;
+	return false;
 }
 
 bool svIsEqual(StringView a, StringView b)
 {
-    if (a.m_count != b.m_count)
-        return false;
+	if(a.m_count != b.m_count) return false;
 
-    if (a.m_count == 0)
-        return true;
+	if(a.m_count == 0) return true;
 
-    return memcmp(a.m_data, b.m_data, a.m_count) == 0;
+	return memcmp(a.m_data, b.m_data, a.m_count) == 0;
+}
+
+bool scIsEqualIgnoreCase(StringView a, StringView b)
+{
+	if(a.m_count != b.m_count)
+	{
+		return false;
+	}
+
+	char x, y;
+	for(size_t i = 0; i < a.m_count; i++)
+	{
+		x = 'A' <= a.m_data[i] && a.m_data[i] <= 'Z' ? a.m_data[i] + 32 : a.m_data[i];
+
+		y = 'A' <= b.m_data[i] && b.m_data[i] <= 'Z' ? b.m_data[i] + 32 : b.m_data[i];
+
+		if(x != y) return false;
+	}
+	return true;
 }
 
 /*
-    *   @brief: the above implementation has an extra safety check
-        for the case when a.m_count == 0. which may be an extra comparison that is not needed.
+	*   @brief: the above implementation has an extra safety check
+		for the case when a.m_count == 0. which may be an extra comparison that is not needed.
 bool svIsEqual(StringView a, StringView b)
 {
-    if (a.m_count != b.m_count)
-        return false;
+	if (a.m_count != b.m_count)
+		return false;
 
-    return memcmp(a.m_data, b.m_data, a.m_count) == 0;
+	return memcmp(a.m_data, b.m_data, a.m_count) == 0;
 }
 */
 
-bool svStartsWith(StringView sv, StringView prefix)
+bool svStartsWith(StringView sv, StringView expected_prefix)
 {
-    if (sv.m_count <= prefix.m_count)
-    {
-        StringView sv_Prefix = svFromParts(sv.m_data, prefix.m_count);
-        return svIsEqual(sv_Prefix, prefix);
-    }
+	if(expected_prefix.m_count <= sv.m_count)
+	{
+		StringView actual_prefix = svFromParts(sv.m_data, expected_prefix.m_count);
+		return svIsEqual(expected_prefix, actual_prefix);
+	}
 
-    return false;
+	return false;
 }
 
 bool svEndsWith(StringView sv, StringView suffix)
 {
-    if(sv.m_count <= suffix.m_count)
-    {
-        StringView sv_Suffix = svFromParts(sv.m_data + sv.m_count - suffix.m_count, suffix.m_count);
-        return svIsEqual(sv_Suffix, suffix);
-    }
+	if(sv.m_count >= suffix.m_count)
+	{
+		StringView sv_Suffix = svFromParts(sv.m_data + sv.m_count - suffix.m_count, suffix.m_count);
+		return svIsEqual(sv_Suffix, suffix);
+	}
 
-    return false;
+	return false;
 }
 
-
-uint64_t StringViewTouint64(StringView sv)
+uint64_t StringViewToU64(StringView sv)
 {
-    uint64_t result = 0;
-    for(size_t i = 0; i < sv.m_count && isdigit(sv.m_data[i]); ++i)
-        result = result * 10 + (sv.m_data[i] - '0');
+	uint64_t result = 0;
+	for(size_t i = 0; i < sv.m_count && isdigit(sv.m_data[i]); ++i)
+		result = result * 10 + (sv.m_data[i] - '0');
 
-    return result;
+	return result;
 }
 
 /*
-    *   @brief: the implementation belows should work logically
-        but remains untested.
+	*   @brief: the implementation belows should work logically
+		but remains untested.
 
-    Todo: test the following implementation.
+	Todo: test the following implementation.
 uint64_t StringViewTouint64(StringView sv)
 {
-    uint64_t result = 0;
-    for (size_t i = 0; i < sv.m_count; i++)
-    {
-        char c = sv.m_data[i];
-        if (c < '0' || c > '9')
-            return 0;
+	uint64_t result = 0;
+	for (size_t i = 0; i < sv.m_count; i++)
+	{
+		char c = sv.m_data[i];
+		if (c < '0' || c > '9')
+			return 0;
 
-        result = result * 10 + (c - '0');
-    }
+		result = result * 10 + (c - '0');
+	}
 
-    return result;
+	return result;
 }
 */
--- src/string_view.h
+++ src/string_view.h
@@ -1,59 +1,57 @@
 #ifndef STRING_VIEW_H
 #define STRING_VIEW_H
 
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 typedef struct StringView
 {
-    size_t m_count;
-    const char* m_data;
-}StringView;
+	size_t m_count;
+	const char* m_data;
+} StringView;
 
 #define SV(cstr_lit) svFromParts(cstr_lit, sizeof(cstr_lit) - 1)
 
-#define SV_STATIC(cstr_lit)     \
-    {                           \
-        sizeof(cstr_lit) - 1,   \
-        cstr_lit                \
-    }
+#define SV_STATIC(cstr_lit)                                                                        \
+	{                                                                                              \
+		sizeof(cstr_lit) - 1, cstr_lit                                                             \
+	}
 
-#define SV_NULL svFromParts(NULL, 0)
+#define SV_NULL	   svFromParts(NULL, 0)
+#define SV_Arg(sv) (int)(sv).m_count, (sv).m_data
 
 // Need a macro for SV for printf
 #define SV_FMT "%.*s"
 
-#define SV_AVERAGE(sv) (int) (sv).m_count, (sv).m_data
+#define SV_AVERAGE(sv) (int)(sv).m_count, (sv).m_data
 /*
-    * @brief: Usage
-    * StringView sv = ...;
-    * printf("Name: SV_FMT", SV_AVERAGE(name));
-*/
+ * @brief: Usage
+ * StringView sv = ...;
+ * printf("Name: SV_FMT", SV_AVERAGE(name));
+ */
 
 // Function Signatures
 
 StringView svFromParts(const char* data, size_t count);
 StringView svFromCStr(const char* cstr);
-
-StringView svTrim(StringView sv);
 StringView svTrimLeft(StringView sv);
 StringView svTrimRight(StringView sv);
-
+StringView svTrim(StringView sv);
 StringView svTakeLeftWhile(StringView sv, bool (*predicate)(char x));
-
-bool try_svChopByDelim(StringView* sv, char delim, StringView* chunk);
 StringView svChopByDelim(StringView* sv, char delim);
-
+StringView svChopBySV(StringView* sv, StringView thicc_delim);
+bool try_svChopByDelim(StringView* sv, char delim, StringView* chunk);
 StringView svChopLeft(StringView* sv, size_t count);
 StringView svChopRight(StringView* sv, size_t count);
 StringView svChopLeftWhile(StringView* sv, bool (*predicate)(char x));
 bool svIndexOf(StringView sv, char c, size_t* index);
 bool svIsEqual(StringView a, StringView b);
+
+bool scIsEqualIgnoreCase(StringView a, StringView b);
+
 bool svStartsWith(StringView sv, StringView prefix);
 bool svEndsWith(StringView sv, StringView suffix);
-uint64_t StringViewTouint64(StringView sv);
+uint64_t StringViewToU64(StringView sv);
 
-
-
-#endif //STRING_VIEW_H
+#endif // STRING_VIEW_H
